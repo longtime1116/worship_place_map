34c2232b491598b3fd56e02583d32e32e3f0ccff
Aiki YOUGAI <phoenix22.you@gmail.com>
2017-04-25 00:14:26 +0900

    add test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+WorshipPlace.create(:place_id => 1, :name => '日枝神社', :is_temple => false, :address => '東京都千代田区永田町2丁目10番5号', :latitude => 35.674722, :longitude => 139.739583)
+WorshipPlace.create(:place_id => 2, :name => '浅草寺', :is_temple => true, :address => '東京都台東区浅草2丁目3番1号', :latitude => 35.714722, :longitude => 139.79675)
