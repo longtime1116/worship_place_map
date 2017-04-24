137d0168c29b713cad89ab9699329301cc714165
Aiki YOUGAI <phoenix22.you@gmail.com>
2017-04-24 22:12:43 +0900

    rails g model WorshipPlace place_id:integer name:string is_temple:boolean address:string latitude:float longitude:float

---
+++ db/migrate/20170424131232_create_worship_places.rb
@@ -0,0 +1,14 @@
+class CreateWorshipPlaces < ActiveRecord::Migration[5.0]
+  def change
+    create_table :worship_places do |t|
+      t.integer :place_id
+      t.string :name
+      t.boolean :is_temple
+      t.string :address
+      t.float :latitude
+      t.float :longitude
+
+      t.timestamps
+    end
+  end
+end
