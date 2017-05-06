c86d88350b3f693318f15d519ac889c9be5443d9
longtime1116s <long.itomochi.time@gmail.com>
2017-05-06 17:07:56 +0900

    検索ボックスを付けた(modelからの検索はまだ)

--- app/controllers/worship_place_controller.rb
+++ app/controllers/worship_place_controller.rb
@@ -2,4 +2,20 @@ class WorshipPlaceController < ApplicationController
   def index
       @worship_places = WorshipPlace.all
   end
+
+  def search
+      @worship_places = WorshipPlace.all
+      post_params
+      # view の index を表示
+      render :action =>"index"
+  end
+
+  private
+  def post_params
+    unless params[:worship_form].blank? then
+      params.require(:worship_form).permit(
+        type: []
+      )
+    end
+  end
 end
