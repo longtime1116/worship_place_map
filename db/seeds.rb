508274b2173c776c889804ad19c346fcdfa5a441
Aiki YOUGAI <phoenix22.you@gmail.com>
2017-05-31 21:30:12 +0900

    add db seed with goshuin_crawler case1

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,24 +6,43 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+require "csv"
+
+
+CSV.foreach('db/worship_places') do |row|
+    WorshipPlace.create(:place_id      => row[0],
+                        :name          => row[1],
+                        :official_name => row[2],
+                        :is_temple     => row[3],
+                        :address       => row[4],
+                        :latitude      => row[5],
+                        :longitude     => row[6])
+end
+CSV.foreach('db/shrines') do |row|
+    Shrine.create(:place_id => row[0],
+                  :rank     => row[1],
+                  :object   => row[2])
+end
+
 # 日枝神社
-place_id = 1
-WorshipPlace.create(:place_id       => place_id,
-                    :name           => '日枝神社',
-                    :official_name  => '日枝神社',
-                    :is_temple      => false,
-                    :address        => '東京都千代田区永田町2丁目10番5号',
-                    :latitude       => 35.674722,
-                    :longitude      => 139.739583)
-Shrine.create(:place_id => place_id,
-              :rank     => '官幣大社',
-              :object   => '大山咋神')
-Service.create(:place_id      => place_id,
+#place_id = 1
+#WorshipPlace.create(:place_id       => place_id,
+#                    :name           => '日枝神社',
+#                    :official_name  => '日枝神社',
+#                    :is_temple      => false,
+#                    :address        => '東京都千代田区永田町2丁目10番5号',
+#                    :latitude       => 35.674722,
+#                    :longitude      => 139.739583)
+#Shrine.create(:place_id => place_id,
+#              :rank     => '官幣大社',
+#              :object   => '大山咋神')
+Service.create(:place_id      => 1,
                :start_time    => '09:00',
                :end_time      => '17:00',
                :is_book_sold  =>  true)
+
 # 浅草寺
-place_id += 1
+place_id = 565
 WorshipPlace.create(:place_id       => place_id,
                     :name           => '浅草寺',
                     :official_name  => '浅草寺',
